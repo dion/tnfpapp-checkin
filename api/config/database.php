cc290f42d8fcac02c2f7fb8ae2a75ade32a75279
Dion Coder <jellopops@gmail.com>
2024-03-16 18:22:17 -0500

    adding dotenv for php

--- api/config/database.php
+++ api/config/database.php
@@ -1,12 +1,22 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 // used to get mysql database connection
 class Database{
-	
-	private $host = "localhost";
-	private $db_name = "tnfp_prod";
-	private $username = "tnfpu";
-	private $password = "Pantry123";
+	private $host;
+	private $db_name;
+	private $username;
+	private $password;
 	public $conn;
+
+	public function __construct() {
+		$this->host = $_ENV['TNFPAPP_DBHOST'] ?? null;
+		$this->db_name = $_ENV['TNFPAPP_DBNAME'] ?? null;
+		$this->username = $_ENV['TNFPAPP_DBUSER'] ?? null;
+		$this->password = $_ENV['TNFPAPP_DBPASS'] ?? null;
+	}
 
 	// get the database connection
 	public function getConnection(){
